fc73c25249fd84206415eb699a95714a096ee96e
Kareem adel <2021170404@cis.asu.edu.eg>
2024-12-16 21:22:10 +0200

    editing authtextfield

--- lib/Widgets/inputs/AuthTextField.dart
+++ lib/Widgets/inputs/AuthTextField.dart
@@ -1,13 +1,14 @@
 import 'package:ecommerce/extensions/string_extensions.dart';
 import 'package:flutter/material.dart';
 
-Widget authTextField(String hint, TextInputType type, bool obsecure,TextEditingController controller) {
+Widget authTextField(String hint, TextInputType type, bool obsecure,
+    TextEditingController controller) {
   return TextFormField(
-    
+    style: const TextStyle(color: Colors.white, fontFamily: "poppinslight"),
     controller: controller,
     validator: (input) {
       if (input == null || input.isEmpty) {
-        return obsecure?"Password can't be null":"email can't be null";
+        return obsecure ? "Password can't be null" : "email can't be null";
       }
       if (obsecure) {
         if (!input.isValidPassword()) {
@@ -25,11 +26,14 @@ Widget authTextField(String hint, TextInputType type, bool obsecure,TextEditingC
     keyboardType: type,
     decoration: InputDecoration(
         filled: true,
-        fillColor: Colors.grey[200],
+        fillColor: Colors.black,
         hintText: hint,
-        hintStyle: TextStyle(color: Colors.grey[800]),
+        hintStyle: const TextStyle(
+          color: Color.fromRGBO(179, 179, 179, 1),
+          fontFamily: "poppinslight",
+        ),
         border: OutlineInputBorder(
-            borderSide: BorderSide.none,
-            borderRadius: BorderRadius.circular(10))),
+            // borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(5))),
   );
 }
